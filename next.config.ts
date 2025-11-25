1e78ddc4067810a95a060d305edc6ef7675ac4ea
Maxence Gauthier <gauthier@redefine.group>
2025-11-25 16:34:46 +0100

    sans eslint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ⛔️ Désactive ESLint pour Vercel/Next build
+  },
 };
 
 export default nextConfig;
